d73fff71936658981fe0c71e7cc89220a90faf0b
Robert Shepley <shepleysound@gmail.com>
2022-08-22 23:24:48 -0700

    Confirm link with locationiq API.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import logo from './map-icon.svg';
 import './App.css';
 import SearchForm from './SearchForm.js'
 import React from 'react';
+import axios, { Axios } from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locationResult: null,
+    }
+  }
+  async handleSearch() {
+    try {
+      const response = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=seattle&format=json`)
+      console.log(response)
+      this.setState({
+        locationResult: response
+      })
+    } catch (error) {
+      console.log(error)
+    }
 
-  handleSearch() {
-    console.log("Hello!")
   }
   render() {
     return (
@@ -25,5 +40,4 @@ class App extends React.Component {
     );
   }
 }
-
 export default App;
